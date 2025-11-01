7042eeb20a67e482ec46cf334d525b9ad79ef380
zaidkhan-code <zaidkhan1357913579@gmail.com>
2025-11-01 13:49:31 +0500

    added updated code

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,9 +9,9 @@ const _geist = Geist({ subsets: ["latin"] });
 const _geistMono = Geist_Mono({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "v0 App",
-  description: "Created with v0",
-  generator: "v0.app",
+  title: "eksplode",
+  description: "Created with eksplode",
+  generator: "eksplode.app",
 };
 
 export default function RootLayout({
@@ -27,5 +27,5 @@ export default function RootLayout({
         <Toaster position="top-right" richColors />
       </body>
     </html>
-  )
+  );
 }
