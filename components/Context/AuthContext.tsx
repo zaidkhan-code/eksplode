c5169a981651398217453e08c31caa2eb924239b
zaidkhan-code <zaidkhan1357913579@gmail.com>
2025-11-05 15:18:39 +0500

    added some change in ui

--- components/Context/AuthContext.tsx
+++ components/Context/AuthContext.tsx
@@ -75,10 +75,8 @@ export const AuthProvider = ({ children }: { children: ReactNode }) => {
         setIsLoading(false);
         if (status) {
           toast.success(res?.message || "Login successful!");
-          if (res?.accessToken)
-            localStorage.setItem("accessToken", res?.accessToken);
-          if (res?.refreshToken)
-            localStorage.setItem("refreshToken", res?.refreshToken);
+          localStorage.setItem("accessToken", res?.tokens?.accessToken);
+          localStorage.setItem("refreshToken", res?.tokens?.refreshToken);
           if (res?.user) {
             localStorage.setItem("user", JSON.stringify(res.user));
             setUser(res.user);
